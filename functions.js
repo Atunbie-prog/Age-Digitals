ab3524001007bb6564a8add1aeef95339775a453
Atunbie-prog <atunbiebunoluwa@gmail.com>
2026-02-07 19:21:14 +0100

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,22 @@
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('navLinks');
+const navItems = document.querySelectorAll('.nav_links a');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navLinks.classList.toggle('active');
+});
+
+navItems.forEach(item => {
+    item.addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        navLinks.classList.remove('active');
+    });
+});
+
+document.addEventListener('click', (e) => {
+    if (!hamburger.contains(e.target) && !navLinks.contains(e.target)) {
+        hamburger.classList.remove('active');
+        navLinks.classList.remove('active');
+    }
+});
